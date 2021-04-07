9fe82aee781374f556882b1853e3875839dd9812
ginestah <68362184+ginestah@users.noreply.github.com>
2021-04-07 12:56:14 -0400

    added solution branch, and my own solution

--- CountDecimals/countDecimals.js
+++ CountDecimals/countDecimals.js
@@ -1,5 +1,13 @@
 //should count the decimal places in a number
 
-function countDecimals(num) {}
+function countDecimals(num) {
+  if (num === 0) {
+    return 0;
+  } else {
+    let str = num.toString().split(".")[1];
+    return str.split("").length;
+  }
+}
+console.log(countDecimals(124.1523));
 
 module.exports = countDecimals;
